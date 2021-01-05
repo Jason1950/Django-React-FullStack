51b8a3ee6ba456a17e948368c39c2253d5425de0
Jason <eric870615@yahoo.com.tw>
2021-01-05 13:24:00 +0800

    init Django 01/05

---
+++ samplebackend/api/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+## for database
+## if you change or update  models/database config
+## please type "python .\manage.py  makemigrations"
---
+++ samplebackend/api/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import time
+import datetime
+
+
+# Create your views here.
+## put code that could responsible for rendering views and api end points
+
+def main(request):
+    datetime_dt = datetime.datetime.today() # 獲得當地時間
+    dateTimeFormat = datetime_dt.strftime("%Y/%m/%d %H:%M:%S")  # 格式化日期(最小單位到秒)
+    # time_delta = datetime.timedelta(hours=3) #時差
+    # new_dt = datetime_dt + time_delta #本地時間加3小時
+    # datetime_format = new_dt.strftime("%Y/%m/%d %H:%M:%S")  # 格式化日期(最小單位到秒)
+    return HttpResponse(f"Hello , time is {dateTimeFormat}!")
+
+
+
