6f20f82463efcf91fa5934948ae5072373d7bcd1
Jason <eric870615@yahoo.com.tw>
2021-01-06 13:40:21 +0800

    add api init Room

--- samplebackend/api/urls.py
+++ samplebackend/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import main
+from .views import main, RoomView
 
 urlpatterns = [
     path('home', main),
     path('', main),
+    path('serializers',RoomView.as_view())
 ]
--- samplebackend/api/views.py
+++ samplebackend/api/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 import time
 import datetime
 
-
 # Create your views here.
 ## put code that could responsible for rendering views and api end points
 
@@ -15,5 +14,11 @@ def main(request):
     # datetime_format = new_dt.strftime("%Y/%m/%d %H:%M:%S")  # 格式化日期(最小單位到秒)
     return HttpResponse(f"Hello , time is {dateTimeFormat}!")
 
+from rest_framework import generics
+from .serializers import RoomSerializers
+from .models import Room
 
-
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializers
+    
